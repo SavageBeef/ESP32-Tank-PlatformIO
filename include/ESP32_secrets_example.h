2ce151c0a7e4892886eb99743798b7b2c5bce0ec
SavageBeef <DanielNicholls18@gmail.com>
2026-03-17 00:17:44 -0400

    ci: fix Secrets example header file
    -Resolve syntax error in ESP32_secrets_example.h file.

--- include/ESP32_secrets_example.h
+++ include/ESP32_secrets_example.h
@@ -2,5 +2,5 @@
 #define Secret_AUTH "auth_token_here"
 #define Secret_SSID "ssid_here"
 #define Secret_PASS "password_here"
-#define Secret_IP 192,168,1,2
+#define Secret_IP "192.168.1.2"
 #define Secret_PORT "8080"
